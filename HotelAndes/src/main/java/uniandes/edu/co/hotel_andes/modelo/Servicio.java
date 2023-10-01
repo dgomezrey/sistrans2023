4b155eb1b82907f7e7a6bdee82bacfcc36fa2551
dgomezrey <d.gomezr2@uniandes.edu.co>
2023-10-01 12:49:27 -0500

    Agregar ServicioENtity

---
+++ HotelAndes/src/main/java/uniandes/edu/co/hotel_andes/modelo/Servicio.java
@@ -0,0 +1,63 @@
+package uniandes.edu.co.hotel_andes.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "servicios")
+public class Servicio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String nombre;
+
+    private String descripcion;
+
+    private String tipoServicio;
+
+    public Servicio(String nombre, String descripcion, String tipoServicio) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.tipoServicio = tipoServicio;
+    }
+
+    public Servicio() {;}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTipoServicio() {
+        return tipoServicio;
+    }
+
+    public void setTipoServicio(String tipoServicio) {
+        this.tipoServicio = tipoServicio;
+    }
+
+}
