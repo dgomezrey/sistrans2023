6db06a08464ef3477d22f9e776410c1bf9a164a6
Tomas Pardo <t.pardo@uniandes.edu.co>
2023-10-01 23:04:14 -0500

    main

--- HotelAndes/src/main/java/uniandes/edu/co/hotel_andes/HotelAndesApplication.java
+++ HotelAndes/src/main/java/uniandes/edu/co/hotel_andes/HotelAndesApplication.java
@@ -1,38 +1,21 @@
 package uniandes.edu.co.hotel_andes;
 
 
-import java.util.Collection;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import uniandes.edu.co.hotel_andes.modelo.Producto;
-
-import uniandes.edu.co.hotel_andes.repositorio.ProductoRepository;
-
 
 @SpringBootApplication
-public class HotelAndesApplication implements CommandLineRunner {
+public class HotelAndesApplication   {
  
-	@Autowired
-	private ProductoRepository ProductoRepository;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelAndesApplication.class, args);
 	}
 
-	@Override
-	public void run (String... arg){
-		Collection <Producto> productos = ProductoRepository.darProductos();
 
-
-		for (Producto s : productos){
-
-			System.out.println(s);
-		}
-
-	}
+	
 
 }
