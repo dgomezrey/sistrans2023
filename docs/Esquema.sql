d64b2fde15f9a0c792684af296e697a00580abd9
dgomezrey <d.gomezr2@uniandes.edu.co>
2023-11-01 07:30:19 -0500

    Nuevos modelos y esquema

--- docs/Esquema.sql
+++ docs/Esquema.sql
@@ -1,255 +1,173 @@
-
-CREATE TABLE cuentaconsumos (
-    reservaalojamientoid NUMBER NOT NULL,
-    servicioid           NUMBER NOT NULL
+CREATE TABLE consumos (
+    id              NUMBER NOT NULL,
+    cantidad        NUMBER NOT NULL,
+    fecha           DATE NOT NULL,
+    total           FLOAT NOT NULL,
+    habitaciones_id NUMBER NOT NULL,
+    servicios_id    NUMBER,
+    productos_id    NUMBER
 );
 
-ALTER TABLE cuentaconsumos ADD CONSTRAINT cuentaconsumos_pk PRIMARY KEY ( reservaalojamientoid,
-                                                                          servicioid );
-
-CREATE TABLE establecimientos (
-    servicioid          NUMBER NOT NULL,
-    tipoestablecimiento VARCHAR2(255 BYTE),
-    estilo              VARCHAR2(255 BYTE),
-    capacidad           VARCHAR2(255 BYTE)
-);
-
-ALTER TABLE establecimientos
-    ADD CHECK ( tipoestablecimiento IN ( 'Bar', 'Restaurante', 'Supermercado', 'Tienda' ) );
-
-ALTER TABLE establecimientos ADD CONSTRAINT establecimientos_pk PRIMARY KEY ( servicioid );
-
-CREATE TABLE gimnasios (
-    servicioid      NUMBER NOT NULL,
-    capacidad       NUMBER,
-    nummaquinas     NUMBER,
-    horarioservicio VARCHAR2(255 BYTE)
-);
-
-ALTER TABLE gimnasios ADD CONSTRAINT gimnasios_pk PRIMARY KEY ( servicioid );
+ALTER TABLE consumos ADD CONSTRAINT consumos_pk PRIMARY KEY ( id );
 
 CREATE TABLE habitaciones (
     id                 NUMBER NOT NULL,
-    numero             NUMBER,
-    piso               NUMBER,
-    reservaalojamiento NUMBER,
-    tipohabitacion     NUMBER NOT NULL
+    numero             NUMBER NOT NULL,
+    piso               NUMBER NOT NULL,
+    ocupada            NUMBER DEFAULT 0 NOT NULL,
+    tiposhabitacion_id NUMBER NOT NULL
 );
+
+ALTER TABLE habitaciones
+    ADD CHECK ( ocupada IN ( 0, 1 ) );
 
 ALTER TABLE habitaciones ADD CONSTRAINT habitaciones_pk PRIMARY KEY ( id );
 
-CREATE TABLE internet (
-    servicioid  NUMBER NOT NULL,
-    capacidad   NUMBER,
-    costopordia NUMBER
-);
-
-ALTER TABLE internet ADD CONSTRAINT internet_pk PRIMARY KEY ( servicioid );
-
-CREATE TABLE lavanderias (
-    servicioid     NUMBER NOT NULL,
-    tipoprenda     VARCHAR2(255 BYTE),
-    costoporprenda FLOAT
-);
-
-ALTER TABLE lavanderias ADD CONSTRAINT lavanderias_pk PRIMARY KEY ( servicioid );
-
 CREATE TABLE ofrecenproductos (
-    establecimientoid NUMBER NOT NULL,
-    productoid        NUMBER NOT NULL
+    servicios_id NUMBER NOT NULL,
+    productos_id NUMBER NOT NULL
 );
 
-ALTER TABLE ofrecenproductos ADD CONSTRAINT ofrecenproductos_pk PRIMARY KEY ( establecimientoid,
-                                                                              productoid );
-
-CREATE TABLE piscinas (
-    servicioid      NUMBER NOT NULL,
-    capacidad       NUMBER,
-    profundidad     FLOAT,
-    horarioservicio VARCHAR2(255 BYTE)
-);
-
-ALTER TABLE piscinas ADD CONSTRAINT piscinas_pk PRIMARY KEY ( servicioid );
+ALTER TABLE ofrecenproductos ADD CONSTRAINT ofrecenproductos_pk PRIMARY KEY ( servicios_id,
+                                                                              productos_id );
 
 CREATE TABLE planesconsumo (
     id          NUMBER NOT NULL,
-    nombre      VARCHAR2(255 BYTE),
-    descripcion VARCHAR2(255 BYTE),
+    nombre      VARCHAR2(255 BYTE) NOT NULL,
+    descripcion VARCHAR2(255 BYTE) NOT NULL,
     descuento   FLOAT
 );
 
 ALTER TABLE planesconsumo ADD CONSTRAINT planesconsumo_pk PRIMARY KEY ( id );
 
-CREATE TABLE prestamos (
-    servicioid    NUMBER NOT NULL,
-    utensilio     VARCHAR2(255 BYTE),
-    costoremplazo FLOAT
-);
-
-ALTER TABLE prestamos ADD CONSTRAINT prestamos_pk PRIMARY KEY ( servicioid );
-
 CREATE TABLE productos (
     id     NUMBER NOT NULL,
-    nombre VARCHAR2(255 BYTE),
-    precio FLOAT
+    nombre VARCHAR2(255 BYTE) NOT NULL,
+    precio FLOAT NOT NULL
 );
 
 ALTER TABLE productos ADD CONSTRAINT productos_pk PRIMARY KEY ( id );
 
 CREATE TABLE reservasalojamiento (
-    id          NUMBER NOT NULL,
-    fecha       DATE,
-    numnoches   NUMBER,
-    numadultos  NUMBER,
-    nummenores  NUMBER,
-    usuario     NUMBER,
-    planconsumo NUMBER
+    id               NUMBER NOT NULL,
+    fechain          DATE NOT NULL,
+    fechaout         DATE NOT NULL,
+    numpersonas      NUMBER NOT NULL,
+    usuarios_id      NUMBER NOT NULL,
+    planesconsumo_id NUMBER,
+    habitaciones_id  NUMBER NOT NULL
 );
 
 ALTER TABLE reservasalojamiento ADD CONSTRAINT reservasalojamiento_pk PRIMARY KEY ( id );
 
 CREATE TABLE reservasservicio (
-    id                 NUMBER NOT NULL,
-    fechainicio        DATE,
-    fechafin           DATE,
-    reservaalojamiento NUMBER,
-    servicio           NUMBER
+    id              NUMBER NOT NULL,
+    fecha           DATE NOT NULL,
+    horainicio      DATE NOT NULL,
+    horafin         DATE NOT NULL,
+    servicios_id    NUMBER NOT NULL,
+    habitaciones_id NUMBER NOT NULL
 );
 
 ALTER TABLE reservasservicio ADD CONSTRAINT reservasservicio_pk PRIMARY KEY ( id );
 
-CREATE TABLE salones (
-    servicioid     NUMBER NOT NULL,
-    tiposalon      VARCHAR2(255 BYTE),
-    tiempolimpieza NUMBER,
-    costoporhora   FLOAT
-);
-
-ALTER TABLE salones
-    ADD CHECK ( tiposalon IN ( 'SalonConferencia', 'SalonReunion' ) );
-
-ALTER TABLE salones ADD CONSTRAINT salones_pk PRIMARY KEY ( servicioid );
-
 CREATE TABLE servicios (
-    id           NUMBER NOT NULL,
-    nombre       VARCHAR2(255 BYTE),
-    descripcion  VARCHAR2(255 BYTE),
-    tiposervicio VARCHAR2(255 BYTE)
+    id             NUMBER NOT NULL,
+    nombre         VARCHAR2(255 BYTE) NOT NULL,
+    descripcion    VARCHAR2(255 BYTE) NOT NULL,
+    tiposervicio   VARCHAR2(255 BYTE) NOT NULL,
+    costoporunidad FLOAT,
+    horario        VARCHAR2(255 BYTE),
+    capacidad      NUMBER
 );
 
 ALTER TABLE servicios
-    ADD CHECK ( tiposervicio IN ( 'EstablecimientoConsumo', 'Gimnasio', 'Internet', 'Lavanderia', 'Piscina',
-                                  'Prestamo', 'SPA', 'Salon' ) );
+    ADD CHECK ( tiposervicio IN ( 'Bar', 'Gimnasio', 'Internet', 'Lavanderia', 'Piscina',
+                                  'Prestamo', 'Restaurante', 'SPA', 'SalonConferencia', 'SalonReunion',
+                                  'Supermercado', 'Tienda' ) );
+
+ALTER TABLE servicios
+    ADD CHECK ( horario IN ( 'manana', 'noche', 'tarde' ) );
 
 ALTER TABLE servicios ADD CONSTRAINT servicios_pk PRIMARY KEY ( id );
 
-CREATE TABLE spas (
-    servicioid NUMBER NOT NULL,
-    duracion   NUMBER,
-    costo      FLOAT
-);
-
-ALTER TABLE spas ADD CONSTRAINT spas_pk PRIMARY KEY ( servicioid );
-
 CREATE TABLE tiposhabitacion (
     id            NUMBER NOT NULL,
-    tipo          VARCHAR2(255 BYTE),
-    capacidad     NUMBER,
-    camas         NUMBER,
-    costopornoche FLOAT
+    tipo          VARCHAR2(255 BYTE) NOT NULL,
+    capacidad     NUMBER NOT NULL,
+    camas         NUMBER NOT NULL,
+    costopornoche FLOAT NOT NULL
 );
 
 ALTER TABLE tiposhabitacion ADD CONSTRAINT tiposhabitacion_pk PRIMARY KEY ( id );
 
 CREATE TABLE tiposusuario (
     id   NUMBER NOT NULL,
-    tipo VARCHAR2(255 BYTE)
+    tipo VARCHAR2(255 BYTE) NOT NULL
 );
 
 ALTER TABLE tiposusuario ADD CONSTRAINT tiposusuario_pk PRIMARY KEY ( id );
 
 CREATE TABLE usuarios (
-    id          NUMBER NOT NULL,
-    nombre      VARCHAR2(255 BYTE),
-    apellido    VARCHAR2(255 BYTE),
-    documento   VARCHAR2(255 BYTE),
-    tipousuario NUMBER
+    id              NUMBER NOT NULL,
+    usuario         VARCHAR2(255 BYTE) NOT NULL,
+    contrasena      VARCHAR2(255 BYTE) NOT NULL,
+    nombre          VARCHAR2(255 BYTE) NOT NULL,
+    email           VARCHAR2(255 BYTE) NOT NULL,
+    tipodocumento   VARCHAR2(255 BYTE) NOT NULL,
+    documento       VARCHAR2(255 BYTE) NOT NULL,
+    tiposusuario_id NUMBER NOT NULL
 );
+
+ALTER TABLE usuarios
+    ADD CHECK ( tipodocumento IN ( 'CC', 'CE', 'LICENSIA', 'OTRO', 'PASAPORTE' ) );
 
 ALTER TABLE usuarios ADD CONSTRAINT usuarios_pk PRIMARY KEY ( id );
 
-ALTER TABLE cuentaconsumos
-    ADD CONSTRAINT cuenta_reservaaloj_fk FOREIGN KEY ( reservaalojamientoid )
-        REFERENCES reservasalojamiento ( id );
+ALTER TABLE consumos
+    ADD CONSTRAINT consumos_habitaciones_fk FOREIGN KEY ( habitaciones_id )
+        REFERENCES habitaciones ( id );
 
-ALTER TABLE cuentaconsumos
-    ADD CONSTRAINT cuenta_servicio_fk FOREIGN KEY ( servicioid )
-        REFERENCES servicios ( id );
-
-ALTER TABLE establecimientos
-    ADD CONSTRAINT establecimiento_servicio_fk FOREIGN KEY ( servicioid )
-        REFERENCES servicios ( id );
-
-ALTER TABLE gimnasios
-    ADD CONSTRAINT gimnasio_servicio_fk FOREIGN KEY ( servicioid )
-        REFERENCES servicios ( id );
-
-ALTER TABLE habitaciones
-    ADD CONSTRAINT habitacion_reservaaloj_fk FOREIGN KEY ( reservaalojamiento )
-        REFERENCES reservasalojamiento ( id );
-
-ALTER TABLE habitaciones
-    ADD CONSTRAINT habitacion_tipohabitacion_fk FOREIGN KEY ( tipohabitacion )
-        REFERENCES tiposhabitacion ( id );
-
-ALTER TABLE internet
-    ADD CONSTRAINT internet_servicio_fk FOREIGN KEY ( servicioid )
-        REFERENCES servicios ( id );
-
-ALTER TABLE lavanderias
-    ADD CONSTRAINT lavanderia_servicio_fk FOREIGN KEY ( servicioid )
-        REFERENCES servicios ( id );
-
-ALTER TABLE ofrecenproductos
-    ADD CONSTRAINT ofreceprod_establecimiento_fk FOREIGN KEY ( establecimientoid )
-        REFERENCES establecimientos ( servicioid );
-
-ALTER TABLE ofrecenproductos
-    ADD CONSTRAINT ofreceprod_producto_fk FOREIGN KEY ( productoid )
+ALTER TABLE consumos
+    ADD CONSTRAINT consumos_productos_fk FOREIGN KEY ( productos_id )
         REFERENCES productos ( id );
 
-ALTER TABLE piscinas
-    ADD CONSTRAINT piscina_servicio_fk FOREIGN KEY ( servicioid )
+ALTER TABLE consumos
+    ADD CONSTRAINT consumos_servicios_fk FOREIGN KEY ( servicios_id )
         REFERENCES servicios ( id );
 
-ALTER TABLE prestamos
-    ADD CONSTRAINT prestamo_servicio_fk FOREIGN KEY ( servicioid )
+ALTER TABLE habitaciones
+    ADD CONSTRAINT habitaciones_tipos_fk FOREIGN KEY ( tiposhabitacion_id )
+        REFERENCES tiposhabitacion ( id );
+
+ALTER TABLE ofrecenproductos
+    ADD CONSTRAINT ofrecenproductos_productos_fk FOREIGN KEY ( productos_id )
+        REFERENCES productos ( id );
+
+ALTER TABLE ofrecenproductos
+    ADD CONSTRAINT ofrecenproductos_servicios_fk FOREIGN KEY ( servicios_id )
         REFERENCES servicios ( id );
 
 ALTER TABLE reservasalojamiento
-    ADD CONSTRAINT reservaaloj_planconsumo_fk FOREIGN KEY ( planconsumo )
+    ADD CONSTRAINT reservasaloj_habi_fk FOREIGN KEY ( habitaciones_id )
+        REFERENCES habitaciones ( id );
+
+ALTER TABLE reservasalojamiento
+    ADD CONSTRAINT reservasaloj_planes_fk FOREIGN KEY ( planesconsumo_id )
         REFERENCES planesconsumo ( id );
 
 ALTER TABLE reservasalojamiento
-    ADD CONSTRAINT reservaalojamiento_usuario_fk FOREIGN KEY ( usuario )
+    ADD CONSTRAINT reservasaloj_usuarios_fk FOREIGN KEY ( usuarios_id )
         REFERENCES usuarios ( id );
 
 ALTER TABLE reservasservicio
-    ADD CONSTRAINT reservaservicio_reservaaloj_fk FOREIGN KEY ( reservaalojamiento )
-        REFERENCES reservasalojamiento ( id );
+    ADD CONSTRAINT reservasserv_hab_fk FOREIGN KEY ( habitaciones_id )
+        REFERENCES habitaciones ( id );
 
 ALTER TABLE reservasservicio
-    ADD CONSTRAINT reservaservicio_servicio_fk FOREIGN KEY ( servicio )
-        REFERENCES servicios ( id );
-
-ALTER TABLE salones
-    ADD CONSTRAINT salon_servicio_fk FOREIGN KEY ( servicioid )
-        REFERENCES servicios ( id );
-
-ALTER TABLE spas
-    ADD CONSTRAINT spa_servicio_fk FOREIGN KEY ( servicioid )
+    ADD CONSTRAINT reservasservicio_servicios_fk FOREIGN KEY ( servicios_id )
         REFERENCES servicios ( id );
 
 ALTER TABLE usuarios
-    ADD CONSTRAINT usuario_tipousuario_fk FOREIGN KEY ( tipousuario )
+    ADD CONSTRAINT usuarios_tiposusuario_fk FOREIGN KEY ( tiposusuario_id )
         REFERENCES tiposusuario ( id );
