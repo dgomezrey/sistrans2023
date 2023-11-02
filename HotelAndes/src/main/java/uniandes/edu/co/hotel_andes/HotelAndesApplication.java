c8563f5500a604c4e63d354c209a4b99216695ee
dgomezrey <d.gomezr2@uniandes.edu.co>
2023-11-02 00:49:59 -0500

    Primer controller

--- HotelAndes/src/main/java/uniandes/edu/co/hotel_andes/HotelAndesApplication.java
+++ HotelAndes/src/main/java/uniandes/edu/co/hotel_andes/HotelAndesApplication.java
@@ -2,19 +2,35 @@ package uniandes.edu.co.hotel_andes;
 
 
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import uniandes.edu.co.hotel_andes.modelo.Servicio;
+import uniandes.edu.co.hotel_andes.repositorio.ServicioRepository;
+
 
 @SpringBootApplication
-public class HotelAndesApplication   {
+public class HotelAndesApplication  implements CommandLineRunner {
  
-	
+	@Autowired
+	private ServicioRepository servicioRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelAndesApplication.class, args);
 	}
 
+	@Override
+	public void run(String... arg) {
+		Collection<Servicio> servicios = servicioRepository.darServicios();
+		for (Servicio servicio : servicios) {
+			System.out.println(servicio);
+		}
+
+	}
 
 	
 
